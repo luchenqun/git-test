94814af0aa4e425f5c4bf32a5995ee454749bdc5
LuChenQun <luchenqun@qq.com>
2015-05-07 11:08:17 +0800

    branchOne commit

---
+++ BranchOne.h
@@ -0,0 +1,8 @@
+#pragma once
+class BranchOne
+{
+public:
+	BranchOne();
+	~BranchOne();
+};
+
--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 #include "master.h"
+#include "BranchOne.h"
 
 int main()
 {
 	Master master;
+	BranchOne branchOne;
 	return 0;
 }
